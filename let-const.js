aef02e74d37c9e0108957c18349080a2f1a1881b
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-20 19:54:00 +0600

    let-const

---
+++ let-const.js
@@ -0,0 +1,12 @@
+/* let allows reassign
+const dose not allows reassign 
+
+both let and const are block scopes
+
+var is a global deceleration method
+value of var hoisting in global */
+
+const name = "rahim";
+let age = 34;
+age = 45;
+var food = "somocha";
