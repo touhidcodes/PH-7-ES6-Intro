37a22ba84086f57b2dd93a901148ebb49c7c3aa5
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-21 17:12:26 +0600

    arrow-function

---
+++ arrow-function.js
@@ -0,0 +1,21 @@
+/* // Function Declaration
+function add(first, second) {
+	const total = first + second;
+	return total;
+} */
+
+/* // Function Expression
+const add1 = function add1(first, second) {
+	const total = first + second;
+	return total;
+}; */
+
+/* // Function Expression Anonymous
+const add2 = function(first, second) {
+	const total = first + second;
+	return total;
+}; */
+
+// Arrow Function
+const add3 = (first, second) => first + second;
+
