56b079e100ad8bda175ecdbd70456f995eac79ab
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-21 13:56:24 +0600

    template-string

---
+++ template.js
@@ -0,0 +1,24 @@
+/* const string1 = "adam";
+const string2 = 'baba';
+const string3 = `powerful`;
+ */
+const oldMultiline = "firsr \n" + "Second \n" + "third \n";
+
+const es6Multiline = `first
+second
+third`;
+
+// console.log(oldMultiline);
+// console.log(es6Multiline);
+
+const a = 23;
+const b = 20;
+const total = `first number is ${a} and second number is ${b} and total is ${
+	a + b
+}`;
+
+// console.log(total);
+
+const array = [2, 4, 5, 6, 3];
+const length = `Array length is ${array.length}`;
+console.log(length);
