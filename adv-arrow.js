2cd34e05a413b78aab2c3f7795fe278a4348091e
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-21 17:24:59 +0600

    arrow-function-adv

---
+++ adv-arrow.js
@@ -0,0 +1,17 @@
+// Single Block Parameter
+const add = (first, second) => first + second;
+
+const multiply = (first, second) => first * second;
+
+// const result = multiply(10, 3);
+// console.log(result);
+
+const num = (num) => num * 2;
+
+// Multiline Arrow Function
+const multi = (a, b, c) => {
+	const add = a + b;
+	const multi = a * c;
+	const total = multi - add;
+	return total;
+};
